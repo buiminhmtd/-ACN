1aadefb28a26151059068516d43b19f6a81e112f
Pham Duy Khanh <142990084+khanhphamduy15@users.noreply.github.com>
2024-10-30 22:13:20 +0700

    updated

--- Payment data/StripeSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace payment.PaymentData
-{
-    public class StripeSettings
-    {
-        public string PublishableKey { get; set; }
-        public string SecretKey { get; set; }
-
-    }
-}
